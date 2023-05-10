e3ef036998adc3a655d42ece02f05b737b099014
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-05-10 18:29:02 -0400

    template created

---
+++ src/infra/http/app.ts
@@ -0,0 +1,26 @@
+import cors from 'cors'
+import { config } from 'dotenv-flow'
+import express from 'express'
+
+config({silent: true})
+
+import { router } from './routes'
+
+const app = express()
+
+app.use(
+  cors({
+    exposedHeaders: ['x-total-count', 'Content-Type', 'Content-Length']
+  })
+)
+
+app.use(
+  express.json({
+    type: ['application/json', 'text/plain']
+  })
+)
+
+app.use(router)
+
+export { app }
+
---
+++ src/modules/Account/domain/user.ts
@@ -0,0 +1,43 @@
+import { Entity } from "@core/domain/Entity";
+import { Either, right } from "@core/logic/Either";
+
+import { Email } from "./email";
+import { Password } from "./password";
+import { Username } from "./username";
+
+import { InvalidEmailError } from "./errors/InvalidEmailError";
+import { InvalidPasswordLengthError } from "./errors/InvalidPasswordLengthError";
+import { InvalidUsernameError } from "./errors/InvalidUsernameError";
+
+interface IUserProps {
+  username: Username
+  email: Email
+  password: Password
+}
+
+export class User extends Entity<IUserProps>{
+  get username() {
+    return this.props.username
+  }
+  
+  get email() {
+    return this.props.email
+  }
+
+  private constructor(props: IUserProps, id?: string) {
+    super(props, id)
+  }
+
+  static create(
+    props: IUserProps, id?: string
+  ): Either<
+    InvalidUsernameError |
+    InvalidEmailError |
+    InvalidPasswordLengthError,
+    User
+  > {
+    const user = new User(props, id)
+
+    return right(user)
+  }
+}
