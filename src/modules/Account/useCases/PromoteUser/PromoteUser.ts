5c75080eaeb022386c0c6fb3be9ee29acff4e1c1
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-06-23 09:19:02 -0400

    ref: change logic

--- src/modules/Account/useCases/PromoteUser/PromoteUser.ts
+++ src/modules/Account/useCases/PromoteUser/PromoteUser.ts
@@ -27,32 +27,46 @@ export class PromoteUser {
   
   async execute({ userId, roleId }: PromoteUserRequest): Promise<PromoteUserResponse>{
 
-    const roleUserOrError = UserRole.create({ roleId, userId })
     
-    
-    if (roleUserOrError.isLeft()) {
-      return left(roleUserOrError.value)
-    }
-    const userExist = await this.userRepository.findById(roleUserOrError.value.userId)
 
+    //busca o usuario com o id
+    const userExist = await this.userRepository.findById(userId)
+
+    //verifica se existe esse usuario
     if (!userExist) {
       return left(new UserNotFoundError())
     }
 
-    const roleExist = await this.roleRepository.findById(roleUserOrError.value.roleId)
+    //Busca o cargo com o id
+    const roleExist = await this.roleRepository.findById(roleId)
 
+    //Verifica se existe esse cargo
     if (!roleExist) {
       return left(new RoleNotFoundError())
     }
+
+    //cria o objeto userRole
+    const roleUserOrError = UserRole.create({ roleId, userId })
+    
+    //verifica se userRole foi validado
+    if (roleUserOrError.isLeft()) {
+      return left(roleUserOrError.value)
+    }
+
     const promoteUser = roleUserOrError.value
+
+    //Busca se existe ja um registro com esse id's
     const promoteAlreadyExists = await this.userRoleRepository.exists(promoteUser.userId, promoteUser.roleId)
 
+    //Verifica se existe
     if (promoteAlreadyExists) {
       return left(new RoleAlreadyExistsError())
     }
 
+    //Regista no banco de dados o registro
     await this.userRoleRepository.create(promoteUser)
 
+    //retorna com sucesso
     return right(promoteUser)    
   }
 }
